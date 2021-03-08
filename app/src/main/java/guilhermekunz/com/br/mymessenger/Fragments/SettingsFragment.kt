cafe0d5c7bc757be12fb556821a0f44f85273be7
Guilherme Butzen Kunz <guilhermebutzenkunz@gmail.com>
2021-03-08 10:10:58 -0300

    Implementing Image Upload

--- app/src/main/java/guilhermekunz/com/br/mymessenger/Fragments/SettingsFragment.kt
+++ app/src/main/java/guilhermekunz/com/br/mymessenger/Fragments/SettingsFragment.kt
@@ -1,13 +1,29 @@
 package guilhermekunz.com.br.mymessenger.Fragments
 
+import android.app.Activity
+import android.app.AlertDialog
+import android.app.Dialog
+import android.app.ProgressDialog
+import android.content.DialogInterface
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.EditText
+import android.widget.Toast
 import androidx.fragment.app.Fragment
+import com.google.android.gms.dynamic.IFragmentWrapper
+import com.google.android.gms.tasks.Continuation
+import com.google.android.gms.tasks.Task
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.FirebaseUser
 import com.google.firebase.database.*
+import com.google.firebase.storage.FirebaseStorage
+import com.google.firebase.storage.StorageReference
+import com.google.firebase.storage.StorageTask
+import com.google.firebase.storage.UploadTask
 import com.squareup.picasso.Picasso
 import guilhermekunz.com.br.mymessenger.ModelClasses.Users
 import guilhermekunz.com.br.mymessenger.R
@@ -18,6 +34,11 @@ class SettingsFragment : Fragment() {
 
     var usersReference: DatabaseReference? = null
     var firebaseUser: FirebaseUser? = null
+    private val requestCode = 438;
+    private var imageUri: Uri? = null
+    private var storageRef: StorageReference? = null
+    private var coverChecker: String? = ""
+    private var socialChecker: String? = ""
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -28,14 +49,16 @@ class SettingsFragment : Fragment() {
         val view = inflater.inflate(R.layout.fragment_settings, container, false)
 
         firebaseUser = FirebaseAuth.getInstance().currentUser
-        usersReference = FirebaseDatabase.getInstance().reference.child("Users").child(firebaseUser!!.uid)
+        usersReference =
+            FirebaseDatabase.getInstance().reference.child("Users").child(firebaseUser!!.uid)
+        storageRef = FirebaseStorage.getInstance().reference.child("User Images")
 
-        usersReference!!.addValueEventListener(object : ValueEventListener{
+        usersReference!!.addValueEventListener(object : ValueEventListener {
             override fun onDataChange(p0: DataSnapshot) {
-                if (p0.exists()){
+                if (p0.exists()) {
                     val user: Users? = p0.getValue(Users::class.java)
 
-                    if (context!=null) {
+                    if (context != null) {
                         view.username_settings.text = user!!.username
                         Picasso.get().load(user.profile).into(view.profile_image_settings)
                         Picasso.get().load(user.cover).into(view.cover_image_settings)
@@ -48,7 +71,136 @@ class SettingsFragment : Fragment() {
             }
         })
 
+        view.profile_image_settings.setOnClickListener {
+            pickImage()
+        }
+
+        view.cover_image_settings.setOnClickListener {
+            coverChecker = "cover"
+            pickImage()
+        }
+
+        view.set_facebook.setOnClickListener {
+            socialChecker = "facebook"
+            setSocialLinks()
+        }
+
+        view.set_instagram.setOnClickListener {
+            socialChecker = "instagram"
+            setSocialLinks()
+        }
+
         return view
+    }
+
+    private fun setSocialLinks() {
+        val builder: AlertDialog.Builder =
+            AlertDialog.Builder(context, R.style.Theme_AppCompat_DayNight_Dialog_Alert)
+
+        if (socialChecker == "facebook") {
+            builder.setTitle("Write URL:")
+        } else {
+            builder.setTitle("Write username:")
+        }
+
+        val editText = EditText(context)
+
+        if (socialChecker == "instagram") {
+            editText.hint = "www.instagram.com"
+        } else {
+            editText.hint = "_guilhermekunz"
+        }
+        builder.setView(editText)
+
+        builder.setPositiveButton("Create", DialogInterface.OnClickListener { dialog, which ->
+            val str = editText.text.toString()
+
+            if (str == "") {
+                Toast.makeText(context, "uploading...", Toast.LENGTH_SHORT).show()
+            } else {
+                saveSocialLink(str)
+            }
+        })
+        builder.setNegativeButton("Cancel", DialogInterface.OnClickListener { dialog, which ->
+            dialog.cancel()
+        })
+    }
+
+    private fun saveSocialLink(str: String) {
+        val mapSocial = HashMap<String, Any>()
+
+        when(socialChecker) {
+            "facebook" ->
+            {
+                mapSocial["facebbok"] = "https://www.facebook.com/$str"
+            }
+            "instagram" ->
+            {
+                mapSocial["instagram"] = "https://www.instagram.com/$str"
+            }
+        }
+        usersReference!!.updateChildren(mapSocial).addOnCompleteListener { task ->
+            if (task.isSuccessful) {
+                Toast.makeText(context, "Updated Successfully!", Toast.LENGTH_SHORT).show()
+            }
+        }
+    }
+
+    private fun pickImage() {
+        val intent = Intent()
+        intent.type = "image/*"
+        intent.action = Intent.ACTION_GET_CONTENT
+        startActivityForResult(intent, requestCode)
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        if (requestCode == this.requestCode && resultCode == Activity.RESULT_OK && data!!.data != null) {
+            imageUri = data.data
+            Toast.makeText(context, "Please write something...", Toast.LENGTH_SHORT).show()
+            uploadImageToDataBase()
+        }
+    }
+
+    private fun uploadImageToDataBase() {
+        val progressBar = ProgressDialog(context)
+        progressBar.setMessage("image is uploading, please wai...")
+        progressBar.show()
+
+        if (imageUri != null) {
+            val fileRef = storageRef!!.child(System.currentTimeMillis().toString() + ".jpg")
+
+            var uploadTask: StorageTask<*>
+            uploadTask = fileRef.putFile(imageUri!!)
+
+            uploadTask.continueWithTask(Continuation<UploadTask.TaskSnapshot, Task<Uri>> { task ->
+                if (!task.isSuccessful) {
+                    task.exception?.let {
+                        throw it
+                    }
+                }
+                return@Continuation fileRef.downloadUrl
+            }).addOnCompleteListener { task ->
+                if (task.isSuccessful) {
+                    val downloadUrl = task.result
+                    val url = downloadUrl.toString()
+
+                    if (coverChecker == "cover") {
+                        val mapCoverImg = HashMap<String, Any>()
+                        mapCoverImg["cover"] = url
+                        usersReference!!.updateChildren(mapCoverImg)
+                        coverChecker = ""
+                    } else {
+                        val mapProfileImg = HashMap<String, Any>()
+                        mapProfileImg["profile"] = url
+                        usersReference!!.updateChildren(mapProfileImg)
+                        coverChecker = ""
+                    }
+                    progressBar.dismiss()
+                }
+            }
+        }
     }
 
 }
